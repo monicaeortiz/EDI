252824f6dbe50d2b0d9a4afdb1bbdbb57e57bcf7
Monica Ortiz <mortiz@greenwichacademy.org>
2020-12-14 13:48:12 -0500

    update website

--- Portfolio/sketch.js
+++ Portfolio/sketch.js
@@ -1,9 +1,7 @@
 function setup() {
-  createCanvas(innerWidth, innerWidth);
-  stroke(220,220,220);
-  strokeWeight(1);
+  createCanvas(innerWidth, innerHeight);
+  stroke(0,0,0);
   frameRate(5);
-  noFill();
 }
 
 function draw() {
@@ -11,35 +9,35 @@ function draw() {
   background(255,255,255);
   var margin = 30;
   var width = innerWidth - 2*margin;
-  var height = innerWidth - 2*margin;
+  var height = innerHeight - 2*margin;
   var spotX = mouseX;//random(0,width);
   var spotY = mouseY;//random(0,height);
-  var radius = innerWidth/3;
+  var radius = 200;
 
-  var shift = 10;
+  var shift = 7;
   var space = 20;
-  var numSquares = 20;
+  var numSquares = 10;
   var sideLen = width/numSquares;
 
   translate(margin,margin);
   for(var y=0; y < height; y = y+sideLen){
     for(var x=0; x < width; x = x+sideLen){
-
-      // vary shift more slowly
-
       if (sqrt(pow(spotX - x,2) + pow(spotY-y,2)) > radius){
         shift = 0;
-        // fill(240,200, 0);
+        fill(240,200, 0);
         quad(x + space + random(-shift, shift),y + space + random(-shift, shift),x+sideLen - space + random(-shift, shift),y + space + random(-shift, shift), x+sideLen - space + random(-shift, shift),y+sideLen - space + random(-shift, shift), x + space + random(-shift, shift),y+sideLen - space + random(-shift, shift));
         shift = 7;
 
       }
       else{
-        // fill(240,random(170,220), 0);
+        fill(240,random(170,220), 0);
         quad(x + space + random(-shift, shift),y + space + random(-shift, shift),x+sideLen - space + random(-shift, shift),y + space + random(-shift, shift), x+sideLen - space + random(-shift, shift),y+sideLen - space + random(-shift, shift), x + space + random(-shift, shift),y+sideLen - space + random(-shift, shift));
       }
 
     }
   }
-  //noLoop();
+
+
+
+  // noLoop();
 }
